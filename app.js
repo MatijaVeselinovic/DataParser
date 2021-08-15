ae7440b2ab854a047b9ae0b337fd89fb279a45d3
matijaveselinovic94@yahoo.com <matijaveselinovic94@yahoo.com>
2021-08-16 01:28:33 +0200

    Final commit for v1.0

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const app = express();
 
 const indexRoutes = require('./routes/index');
 
+const ignoreFavicon = require('./utils/helperFunctions');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+//Didn't know which favicon to pick so decided to get rid of the console error that was bugging me :O
+app.use(ignoreFavicon);
 app.use(indexRoutes);
 
 app.use(express.static(path.join(__dirname, "css")));
--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -5,3 +5,10 @@ module.exports = findUniqueEntries = (value, index, self) => {
 module.exports = sortArray = (unsortedArray) => {
     return unsortedArray.sort((currentPath, nextPath) => nextPath.occurrence - currentPath.occurrence);
 }
+
+module.exports = ignoreFavicon = (req, res, next) => {
+    if (req.originalUrl.includes('favicon.ico')) {
+      res.status(204).end()
+    }
+    next();
+}
